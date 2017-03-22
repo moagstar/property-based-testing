a029296d0bb7dc5bbe998b39c1a15a46c940fc1f
Daniel Bradburn <moagstar@gmail.com>
2017-03-22 21:00:07 +0100

    started adding data generation cells

--- src/test_3_data_generation.py
+++ src/test_3_data_generation.py
@@ -6,23 +6,65 @@
 # source: "## Data generation"
 #%
 
+#%
+# cell_type: code
+# metadata:
+#   slideshow:
+#     slide_type: subslide
+#%
 from hypothesis import strategies as st
 
-# built in strategies
 st.integers().example()
-st.text().example()
-st.floats().example()
-st.lists(st.integers()).example()
+#%
 
-# recursive strategies
+#%
+# cell_type: code
+# metadata:
+#   slideshow:
+#     slide_type: "-"
+#%
+st.text().example()
+#%
+
+#%
+# cell_type: code
+# metadata:
+#   slideshow:
+#     slide_type: "-"
+#%
+st.floats().example()
+#%
+
+#%
+# cell_type: code
+# metadata:
+#   slideshow:
+#     slide_type: "-"
+#%
+st.lists(st.integers()).example()
+#%
+
+#%
+# cell_type: code
+# metadata:
+#   slideshow:
+#     slide_type: subslide
+#%
 nodes = st.floats() | st.booleans() | st.text() | st.none()
 children = lambda x: st.lists(x) | st.dictionaries(st.text(), x)
 st.recursive(nodes, children).example()
+#%
 
-# composite strategies
+#%
+# cell_type: code
+# metadata:
+#   slideshow:
+#     slide_type: subslide
+#%
 @st.composite
 def composite_strategy(draw):
     pass
+#%
 
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
